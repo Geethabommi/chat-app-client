6a155758e1de7421b7f3dee92466a8ecf1cfef7c
Geethabommi <geethabommi139@gmail.com>
2022-06-22 21:22:59 +0530

    initial commit

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = 'http://localhost:5000';
+export const loginRoute = `${process.env.HOST}/api/auth/login`;
+export const registerRoute = `${process.env.HOST}/api/auth/register`;
+export const logoutRoute = `${process.env.HOST}/api/auth/logout`;
+export const allUsersRoute = `${process.env.HOST}/api/auth/allusers`;
+export const sendMessageRoute = `${process.env.HOST}/api/messages/addmsg`;
+export const recieveMessageRoute = `${process.env.HOST}/api/messages/getmsg`;
+export const setAvatarRoute = `${process.env.HOST}/api/auth/setavatar`;
